da7b2c461ca56487e77a38b52c9156a4820a7648
Alliah Mae <adhisuan@up.edu.ph>
2024-10-08 02:38:16 +0800

    delete_added

--- website1/views.py
+++ website1/views.py
@@ -58,3 +58,14 @@ def song_record(request, pk):
 	else: 
 		messages.success(request, "You must be logged in to view page.")
 		return redirect('home')
+
+
+def delete_record(request, pk):
+	if request.user.is_authenticated: 
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record deleted succesfully.")
+		return redirect('home')
+	else: 
+		messages.success(request, "Log in first to delete record.")
+		return redirect('home')

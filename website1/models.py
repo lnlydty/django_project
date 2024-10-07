f77c96900cc1f3dcc05502fa05a8206d2a6dd820
Alliah Mae <adhisuan@up.edu.ph>
2024-10-08 02:31:22 +0800

    record_page

--- website1/models.py
+++ website1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True) # sets to current date and time when the record is first created
+	song_title = models.CharField(max_length=150)
+	artist_name = models.CharField(max_length=100)
+	song_lyrics = models.TextField()
+	song_type = models.CharField(max_length=50)
+	song_chords = models.TextField()
+
+	def __str__(self):
+		return(f"{self.song_title} {self.artist_name}")
+		
--- website1/views.py
+++ website1/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
+
 def home(request):
+	records = Record.objects.all()
 
 	# check if logging in
 	if request.method == 'POST':
 		username = request.POST['username']
 		password = request.POST['password']
-
 		#Authenticate
 		user = authenticate(request, username=username, password=password)
 
@@ -21,11 +23,9 @@ def home(request):
 		else:
 			messages.success(request, "There was an error logging in, please try again.")
 			return redirect('home')
-	
 	else:
-		return render(request, 'home.html', {})
+		return render(request, 'home.html', {'records':records})
 
-	return render(request, 'home.html', {})
 
 def logout_user(request):
 	logout(request)
@@ -49,3 +49,12 @@ def register_user(request):
 
 
 	return render(request, 'register.html', {'form':form})
+
+
+def song_record(request, pk):
+	if request.user.is_authenticated: 
+		song_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'song_record':song_record})
+	else: 
+		messages.success(request, "You must be logged in to view page.")
+		return redirect('home')
